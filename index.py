48f6a874291a588bf3be40598c4970453e54656d
codycjy <cody741963@protonmail.com>
2020-11-21 00:05:22 +0800

    一个初步的完整概念，待修改

--- index.py
+++ index.py
@@ -2,9 +2,10 @@ import requests
 from lxml import etree
 import re
 import time
-"""cookie='__cfduid=dc8603d98c1b7027705a38d798f593e9c1605241305; _ga=GA1.2.1659073861.1605241306; _gid=GA1.2.1028594813.1605508838; Hm_lvt_ed618df76d54605d45b765fc0ce862e6=1605250661,1605254430,1605508838,1605528938; Hm_lpvt_ed618df76d54605d45b765fc0ce862e6=1605528941'
+cookie='__cfduid=dc8603d98c1b7027705a38d798f593e9c1605241305; _ga=GA1.2.1659073861.1605241306; _gid=GA1.2.1335797918.1605886691; Hm_lvt_ed618df76d54605d45b765fc0ce862e6=1605254430,1605508838,1605528938,1605886691; Hm_lpvt_ed618df76d54605d45b765fc0ce862e6=1605886697'
 ua='User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36'
 headers={'cookie':cookie,'user-agent':ua}
+"""
 for i in range(178, 201):
     url = "http://www.acgjc.com/yy/page/{}/".format(i)
     mainpage = requests.get(url=url,headers=headers).text
@@ -28,15 +29,19 @@ def x1(line):
     page=requests.get(url=line).text
     html0=etree.HTML(page)
     #link0=html0.xpath('//*[@id="post-7594"]/div/div[3]/a[1]')
+    #name=html0.xpath('//*[@id="post-112752"]/h2[@class="entry-title"]/text()')
+    #print(name)
     b=re.findall('(http://www.acgjc.com/storage-download/.*)" target="_blank',page)
     print(b[0])
     with open("p.txt", "a+", encoding="utf-8") as f:
         f.write(b[0])
         f.write("\n")
     time.sleep(2)
+    #return name
 
+#x1("http://www.acgjc.com/yy/112752.html")
 
-with open("index2.txt") as f:
+"""with open("index2.txt") as f:
     x=0
     for line in f:
         try:
@@ -46,13 +51,37 @@ with open("index2.txt") as f:
         except:
             x += 1
             print(x,"failed")
+"""
+#url3='http://www.acgjc.com/storage-download/?code=OTdmMVFDeWZLZ1RnbDV5RUNWbTJGeHB0amdmTU9FL3FPVmRQTmQ4Y0RuL0JydW9lVk5DN0Q4L0Z5TVRUMVVr'
 
-url3='http://www.acgjc.com/storage-download/?code=OTdmMVFDeWZLZ1RnbDV5RUNWbTJGeHB0amdmTU9FL3FPVmRQTmQ4Y0RuL0JydW9lVk5DN0Q4L0Z5TVRUMVVr'
+def x2(url3):
+    time.sleep(5)
+    links=requests.get(url3).text
+    html=etree.HTML(links)
+    tiqu=html.xpath('//*[@id="theme_custom_storage-0-download-pwd"]/@value')
+    print(tiqu)
+    #print(links)
+    link=html.xpath('/html/body/div[4]/div/div[3]/fieldset/div/div[2]/a/@href')
+    print(link)
+    name = html.xpath('/html/body/div[4]/div/div[1]/h2/a/text()')
+    print(name)
 
-"""links=requests.get(url3).text
-html=etree.HTML(links)
-tiqu=html.xpath('//*[@id="theme_custom_storage-0-download-pwd"]/@value')
-print(tiqu)
-#print(links)
-link=html.xpath('/html/body/div[4]/div/div[3]/fieldset/div/div[2]/a/@href')
-print(link)"""
+    return link[0],tiqu[0],name[0]
+
+def x3(url3):
+    with open ('final_test.csv',"a+",encoding="utf-8-sig") as f2:
+        l,t,n=x2(url3)
+        f2.write("{},{},{}".format(n,l,t))
+        f2.write("\n")
+
+#x3('http://www.acgjc.com/storage-download/?code=MGMyZWRlQnRNSkJRcGhYaXROdklIMVA4SS9xdC93Z1dtMExsQVVMVTkvaHJBU25hbitzaHpyc0s4VHpRRzRV')
+"""
+with open("p.txt","r") as f:
+    c=0
+    for line2 in f:
+        c+=1
+        try:
+            x3(line2.strip())
+        except:
+            print("f")
+        print(c)"""
